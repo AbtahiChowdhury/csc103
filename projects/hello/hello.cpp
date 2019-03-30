b7ec939c422f746ba3390d7948935d1faf6baf18
AbtahiChowdhury <45293301+AbtahiChowdhury@users.noreply.github.com>
2019-03-30 13:47:21 -0400

    Add files via upload

---
+++ projects/hello/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name, relative;
+    cout << "What is your name?\n";
+    cin >> name;
+    cout << "Enter a relative\n";
+    cin >> relative;
+    cout << "I, "<<name<<", was burnt by "<<relative<<"'s flaming laser sword.\n";
+    return 0;
+}
